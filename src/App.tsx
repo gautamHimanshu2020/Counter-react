4f665571a6af6d3900c5b5e94df4b97187dc5535
gautamHimanshu2020 <76090929+gautamHimanshu2020@users.noreply.github.com>
2023-06-29 01:08:51 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './style.css';
+import InputComponent from './InputComponent';
+
+export default function App() {
+  return (
+    <div>
+      <div>
+        <InputComponent />
+      </div>
+    </div>
+  );
+}
---
+++ src/InputComponent.jsx
@@ -0,0 +1,30 @@
+import './style.css';
+import React, { useState } from 'react';
+
+export default function InputComponent() {
+  const [inputData, setInputData] = useState('');
+  const [counter, setCounter] = useState(0);
+  function updateInput(event) {
+    setInputData(event.target.value);
+  }
+  function increment() {
+    setCounter((prevCounter) => prevCounter + 1);
+  }
+  function decrement() {
+    setCounter((prevCounter) => prevCounter - 1);
+  }
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      {/* input field value update */}
+      {inputData && <p>{inputData && inputData}</p>}
+      <input id="inputText" className="inputText" onChange={updateInput} />
+
+      <div className="container">
+        <button onClick={() => (counter > 0 ? decrement() : '')}>-</button>
+        <span>{counter}</span>
+        <button onClick={increment}>+</button>
+      </div>
+    </div>
+  );
+}
